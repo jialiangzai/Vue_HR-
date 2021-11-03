a10bd25fd0e4367bd1d61e29958004be20e9d78c
zuishuainanhai <1705902650@qq.com>
2021-11-03 22:44:52 +0800

    first项目初始化

---
+++ src/settings.js
@@ -0,0 +1,16 @@
+module.exports = {
+
+  title: '人资中台',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: true,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: true
+}
