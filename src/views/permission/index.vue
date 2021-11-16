176b0de4477479e88511665cec8b5699b1b4728e
zuishuainanhai <1705902650@qq.com>
2021-11-16 09:21:23 +0800

    权限准备

--- src/views/permission/index.vue
+++ src/views/permission/index.vue
@@ -1,7 +1,30 @@
 <template>
-  <div>权限管理</div>
+  <div class="permission-container">
+    <div class="app-container">
+      <el-card shadow="always" :body-style="{ padding: '20px' }">
+        <div slot="header">
+          <el-row type="flex" justify="space-between" align="middle">
+            <span>权限管理</span>
+            <el-button type="primary">添加权限</el-button>
+          </el-row>
+        </div>
+        <!-- card body 权限点树形table列表 -->
+        <el-table border>
+          <el-table-column label="名称" />
+          <el-table-column label="标识" />
+          <el-table-column label="描述" />
+          <el-table-column label="操作">
+            <template>
+              <el-button type="text">添加</el-button>
+              <el-button type="text">编辑</el-button>
+              <el-button type="text">删除</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+      </el-card>
+    </div>
+  </div>
 </template>
-
 <script>
 export default {}
 </script>
